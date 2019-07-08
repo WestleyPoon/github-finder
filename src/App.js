46db1d7c624addccf23bb3cae650fc4bd29629bf
WestleyPoon <poonw@uci.edu>
2019-07-08 16:07:51 -0700

    added user and spinner components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <Navbar/>
-        </div>
-    );
-  }
+    state = {
+        users: [],
+        loading: false
+    };
+
+    async componentDidMount() {
+        this.setState({loading: true});
+
+        const resp = await axios.get('https://api.github.com/users');
+
+        this.setState({
+            users: resp.data,
+            loading: false
+        });
+    }
+
+    render() {
+        const {users, loading} = this.state;
+        return (
+            <div className="App">
+                <Navbar/>
+                <div className="container">
+                    <Users loading={loading} users={users}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
